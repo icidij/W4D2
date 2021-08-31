ea3827e0582e35bde04d9f4a3ee9c48efed6c597
Zeki <zckural@gmail.com>
2021-08-31 17:33:04 -0400

    null piece

--- Chess/piece.rb
+++ Chess/piece.rb
@@ -1,5 +1,6 @@
 class Piece
-    attr_reader :color
+  attr_reader :color
+
   def initialize(color, board, pos)
     @color = color
     @board = board
@@ -18,58 +19,4 @@ class Piece
     return "white" if @color == "black"
     nil
   end
-
-end
-
-
-class Pawn < Piece
-    
-    def initialize(color, board, pos)
-        super
-        @symbol = "P"
-    end
-
-    def symbol
-        @symbol
-    end
-
-    def move_dirs
-        @pos
-
-    end
-
-    private
-    def at_start_row?
-        return true if ((@color == "white") && (@pos[0] == 6)) || ((@color == "black") && (@pos[0] == 1))
-
-        false
-    end
-    def forward_dir
-
-        return -1 if @color == "white" 
-        return 1 if @color == "black"
-
-    end
-    def forward_steps
-
-        if at_start_row? 
-            return 2 * forward_dir
-        else
-            return forward_dir
-        end
-
-
-    end
-    def side_attacks
-        left_attack = [@pos[0] + forward_dir, (@pos[1] - 1)]
-        right_attack = [@pos[0] + forward_dir, (@pos[1] + 1)]
-        options = []
-        if @board[right_attack].color == @opposite_color
-            options << right_attack
-        elsif @board[left_attack].color == @opposite_color
-            options << left_attack
-        end
-        options
-    end
-
 end
--- Chess/slideable.rb
+++ Chess/slideable.rb
@@ -1,7 +1,7 @@
 module Slideable
 
-    HORIZONTAL_DIRS = [[0,1],]
-    DIAGONAL_DIRS =
+    HORIZONTAL_DIRS = [[0,1],[0,-1],[1,0],[-1,0]]
+    DIAGONAL_DIRS = [[1,1],[1,-1],[-1,1],[-1,-1]]
 
     def move
 
