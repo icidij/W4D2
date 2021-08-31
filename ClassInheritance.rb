4fbfe89b6819c86d0670701b427cb9191e6887ce
Zeki <zckural@gmail.com>
2021-08-31 13:33:02 -0400

    Employee progress

--- ClassInheritance.rb
+++ ClassInheritance.rb
@@ -1,1 +1,18 @@
-#pushed
+
+
+class Employee
+
+    attr_reader :name,:salary,:title
+    def initialize(name,salary,title)
+        @name = name
+        @salary = salary
+        @title = title
+    end
+
+    def bonus(multiplier)
+        @salary * multiplier
+    end
+
+
+
+end
