eff3aa8193c0640f7662db839e797425c85e64a8
icidij <jamesc1288@gmail.com>
2021-08-31 16:58:12 -0400

    add Pawn class and helpers

--- Chess/piece.rb
+++ Chess/piece.rb
@@ -1,8 +1,10 @@
 class Piece
+    attr_reader :color
   def initialize(color, board, pos)
     @color = color
     @board = board
     @pos = pos
+    @opposite_color = opposite_color
   end
 
   def to_s
@@ -10,11 +12,18 @@ class Piece
 
   def moves(pos)
   end
+
+  def opposite_color
+    return "black" if @color == "white"
+    return "white" if @color == "black"
+    nil
+  end
+
 end
 
 
 class Pawn < Piece
-  
+    
     def initialize(color, board, pos)
         super
         @symbol = "P"
@@ -30,5 +39,37 @@ class Pawn < Piece
     end
 
     private
+    def at_start_row?
+        return true if ((@color == "white") && (@pos[0] == 6)) || ((@color == "black") && (@pos[0] == 1))
+
+        false
+    end
+    def forward_dir
+
+        return -1 if @color == "white" 
+        return 1 if @color == "black"
+
+    end
+    def forward_steps
+
+        if at_start_row? 
+            return 2 * forward_dir
+        else
+            return forward_dir
+        end
+
+
+    end
+    def side_attacks
+        left_attack = [@pos[0] + forward_dir, (@pos[1] - 1)]
+        right_attack = [@pos[0] + forward_dir, (@pos[1] + 1)]
+        options = []
+        if @board[right_attack].color == @opposite_color
+            options << right_attack
+        elsif @board[left_attack].color == @opposite_color
+            options << left_attack
+        end
+        options
+    end
 
 end
