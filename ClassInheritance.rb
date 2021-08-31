b12a11b02312757da97389e49570dc37c3974531
icidij <jamesc1288@gmail.com>
2021-08-31 13:57:31 -0400

    Class Inheritance project complete.

--- ClassInheritance.rb
+++ ClassInheritance.rb
@@ -3,10 +3,16 @@
 class Employee
 
     attr_reader :name,:salary,:title
-    def initialize(name,salary,title)
+    def initialize(name,salary,title, manager = nil)
         @name = name
         @salary = salary
         @title = title
+        @manager = assign_manager(manager) if manager != nil
+    end
+
+    def assign_manager(manager)
+        @manager = manager
+        @manager.add_employee(self)
     end
 
     def bonus(multiplier)
@@ -14,5 +20,29 @@ class Employee
     end
 
 
+
+end
+
+class Manager < Employee
+    attr_reader :employees
+    def initialize(name, salary, title, manager = nil)
+        super
+        @employees = []
+    end
+
+
+    def bonus(multiplier)
+        sum = 0
+        @employees.each do |employee|
+
+            sum += employee.salary
+        end
+
+        sum * multiplier
+    end
+
+    def add_employee(employee)
+        @employees << employee
+    end
 
 end
